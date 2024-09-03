0aef257a766c3de417fcae15f084f054390db1ef
Angel Andrade <angelomar.andradelinares@gmail.com>
2024-09-03 13:03:29 -0600

    Se agregaron mas funciones, solo falta habilitar el boton de limpiar todo

--- js/main.js
+++ js/main.js
@@ -3,6 +3,20 @@ const txtNombre = document.getElementById("Name")
 const txtNumber = document.getElementById("Number")
 const alertValidaciones = document.getElementById("alertValidaciones")
 const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+const tablaListaCompras = document.getElementById("tablaListaCompras");
+const cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0);
+const contadorProductos = document.getElementById("contadorProductos");
+const productosTotal = document.getElementById("productosTotal")
+const precioTotal = document.getElementById("precioTotal")
+
+//Bandera, al ser true permite agregar los datos a la tabla 
+let isValid = true;
+
+let contador = 0;
+let precio = 0;
+let costoTotal = 0;
+let totalEnProductos = 0;
+
 
 function validarCantidad() {
     if (txtNumber.value.length == 0) {
@@ -12,12 +26,17 @@ function validarCantidad() {
         return false
     }//isNAN
 
-    if (Number(txtNumber.value)<=0) {
+    if (Number(txtNumber.value) <= 0) {
         return false
     }//<=0
     return true;
 
 }//validar cantidad
+
+function getPrecio() {
+    return Math.round((Math.random() * 10000)) / 100;
+}//Get Precio
+
 
 btnAgregar.addEventListener("click", function (event) {
     event.preventDefault();
@@ -25,11 +44,13 @@ btnAgregar.addEventListener("click", function (event) {
     txtNumber.style.border = "";
     alertValidacionesTexto.innerHTML = "";
     alertValidaciones.style.display = "none";
+    isValid = true;
     //Validar el nombre del producto
     if (txtNombre.value.length < 3) {
         txtNombre.style.border = "solid red medium"
         alertValidacionesTexto.innerHTML = "El <strong>Nombre</strong> no es correcto. <br/>";
         alertValidaciones.style.display = "block";
+        isValid = false
         //return false;
     }// if lenght<3
 
@@ -38,9 +59,44 @@ btnAgregar.addEventListener("click", function (event) {
         txtNumber.style.border = "solid red medium"
         alertValidacionesTexto.innerHTML += "La <strong>Cantidad</strong> no es correcta. <br/>";
         alertValidaciones.style.display = "block";
-    }
+        isValid = false
+    }//! validarCantidad
+
+    if (isValid) {
+        contador++
+        precio = getPrecio();
+        let row = `<tr>
+                        <td>${contador}</td>
+                        <td>${txtNombre.value}</td>
+                        <td>${txtNumber.value}</td>
+                        <td>$${precio}</td>
+        </tr>`;
+        cuerpoTabla.insertAdjacentHTML("beforeend", row);
+
+
+        costoTotal += precio * Number(txtNumber.value);
+        totalEnProductos += Number(txtNumber.value)
+
+        contadorProductos.innerText = contador;
+        productosTotal.innerText = totalEnProductos;
+        precioTotal.innerText = "$" + costoTotal;
+
+        localStorage.setItem("contador", contador);
+        localStorage.setItem("totalEnProductos", totalEnProductos);
+        localStorage.setItem("costoTotal", costoTotal);
+
+        txtNombre.value = "";
+        txtNumber.value = "";
+        txtNombre.focus();
+
+
+
+    }//isValid
+
 
 }); // btnAgregar.addEventListener
+
+
 // evento blur es cuando un campo pierde el foco, se sale del campo 
 
 txtNombre.addEventListener("blur", function (event) {
@@ -50,3 +106,26 @@ txtNombre.addEventListener("blur", function (event) {
 txtNumber.addEventListener("blur", function (event) {
     txtNumber.value = txtNumber.value.trim();
 }); // txtNombre.addEventListener
+
+
+
+
+
+window.addEventListener("load", function () {
+    if  (this.localStorage.getItem("contador") != null) {
+        contador = Number(this.localStorage.getItem("contador"));         
+    }//!null
+    if  (localStorage.getItem("totalEnProductos") != null) {
+        totalEnProductos = Number(localStorage.getItem("totalEnProductos"));         
+    }//!null
+    if  (localStorage.getItem("costoTotal") != null) {
+        costoTotal = Number(localStorage.getItem("costoTotal"));         
+    }//!null
+
+
+
+    localStorage.setItem("contador", contador);
+    localStorage.setItem("totalEnProductos", totalEnProductos);
+    localStorage.setItem("costoTotal", costoTotal);
+
+}) //windows load
